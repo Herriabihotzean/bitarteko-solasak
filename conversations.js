8d7001d2395038d45e411e42c928c100c63d96fb
Herriabihotzean <herriabihotzean64@gmail.com>
2026-07-08 23:26:43 +0200

    Replace 'audio' with 'driveId' in conversations.js

--- conversations.js
+++ conversations.js
@@ -7,306 +7,306 @@ const conversations = [
   {
     numero: 2,
     titre: "Conversation en chemin de fer",
-    audio: "002.mp3"
+    driveId: "002.mp3"
   },
    {
     numero: 3,
     titre: "Conversation en voiture",
-    audio: "003.mp3"
+    driveId: "003.mp3"
   },
 {
     numero: 4,
     titre: "Conversation en bateau",
-    audio: "004.mp3"
+    driveId: "004.mp3"
   },
 {
     numero: 5,
     titre: "Pour demander ses bagages, les faire visiter et montrer son passeport",
-    audio: "005.mp3"
+    driveId: "005.mp3"
   },
   {
     numero: 6,
     titre: "Pour accompagner un étranger qui vient d'arriver jusqu'à son hôtel",
-    audio: "006.mp3"
+    driveId: "006.mp3"
   },
    {
     numero: 7,
     titre: "Pour aider un étranger dans ses recherches",
-    audio: "007.mp3"
+    driveId: "007.mp3"
   },
 {
     numero: 8,
     titre: "Pour louer une maison de campagne",
-    audio: "008.mp3"
+    driveId: "008.mp3"
   },
 {
     numero: 9,
     titre: "Pour louer un appartement",
-    audio: "009.mp3"
+    driveId: "009.mp3"
   },
 {
     numero: 10,
     titre: "Pour demander une chambre dans une auberge",
-    audio: "010.mp3"
+    driveId: "010.mp3"
   },
   {
     numero: 11,
     titre: "Pour remiser un cheval, une voiture",
-    audio: "011.mp3"
+    driveId: "011.mp3"
   },
   {
     numero: 12,
     titre: "Pour demander à manger ou à boire",
-    audio: "012.mp3"
+    driveId: "012.mp3"
   },
    {
     numero: 13,
     titre: "Conversation à table",
-    audio: "013.mp3"
+    driveId: "013.mp3"
   },
 {
     numero: 14,
     titre: "Sur le lever",
-    audio: "014.mp3"
+    driveId: "014.mp3"
   },
 {
     numero: 15,
     titre: "Sur le coucher",
-    audio: "015.mp3"
+    driveId: "015.mp3"
   },
   {
     numero: 16,
     titre: "Pour écrire ou faire écrire une lettre",
-    audio: "016.mp3"
+    driveId: "016.mp3"
   },
    {
     numero: 17,
     titre: "Pour voir une ville et ses environs",
-    audio: "017.mp3"
+    driveId: "017.mp3"
   },
 {
     numero: 18,
     titre: "Pour faire une promenade",
-    audio: "018.mp3"
+    driveId: "018.mp3"
   },
 {
     numero: 19,
     titre: "Sur le jeu de cartes",
-    audio: "019.mp3"
+    driveId: "019.mp3"
   },
 {
     numero: 20,
     titre: "Sur la chasse, la pêche, le jeu de paume",
-    audio: "020.mp3"
+    driveId: "020.mp3"
   },
   {
     numero: 21,
     titre: "Le salut et la bienvenue à un ami, à une connaissance",
-    audio: "021.mp3"
+    driveId: "021.mp3"
   },
   {
     numero: 22,
     titre: "Le congé et l’adieu",
-    audio: "022.mp3"
+    driveId: "022.mp3"
   },
    {
     numero: 23,
     titre: "Pour demander de l'argent",
-    audio: "023.mp3"
+    driveId: "023.mp3"
   },
 {
     numero: 24,
     titre: "Pour faire faire quelque démarche",
-    audio: "024.mp3"
+    driveId: "024.mp3"
   },
 {
     numero: 25,
     titre: "Pour obtenir un emploi",
-    audio: "025.mp3"
+    driveId: "025.mp3"
   },
   {
     numero: 26,
     titre: "Pour demander un avis",
-    audio: "026.mp3"
+    driveId: "026.mp3"
   },
    {
     numero: 27,
     titre: "Pour remercier",
-    audio: "027.mp3"
+    driveId: "027.mp3"
   },
 {
     numero: 28,
     titre: "Conversation sur la paix et la guerre",
-    audio: "028.mp3"
+    driveId: "028.mp3"
   },
 {
     numero: 29,
     titre: "Idem. Sur les mariages",
-    audio: "029.mp3"
+    driveId: "029.mp3"
   },
 {
     numero: 30,
     titre: "Idem. Sur les accidents, les maladies et les morts",
-    audio: "030.mp3"
+    driveId: "030.mp3"
   },
   {
     numero: 31,
     titre: "Idem. Sur les récoltes, les vendanges et le cours de divers prix",
-    audio: "031.mp3"
+    driveId: "031.mp3"
   },
   {
     numero: 32,
     titre: "Idem. Sur le temps",
-    audio: "032.mp3"
+    driveId: "032.mp3"
   },
    {
     numero: 33,
     titre: "Idem. Sur l'heure",
-    audio: "033.mp3"
+    driveId: "033.mp3"
   },
 {
     numero: 34,
     titre: "Idem. Sur le français et le basque",
-    audio: "034.mp3"
+    driveId: "034.mp3"
   },
 {
     numero: 35,
     titre: "Idem. Pour affirmer et nier une chose",
-    audio: "035.mp3"
+    driveId: "035.mp3"
   },
   {
     numero: 36,
     titre: "Idem. Pour exprimer le doute, la surprise, l'admiration",
-    audio: "036.mp3"
+    driveId: "036.mp3"
   },
    {
     numero: 37,
     titre: "Idem. Pour témoigner la joie et l'affliction",
-    audio: "037.mp3"
+    driveId: "037.mp3"
   },
 {
     numero: 38,
     titre: "Idem. Pour faire des reproches, pour menacer, pour exprimer sa colère",
-    audio: "038.mp3"
+    driveId: "038.mp3"
   },
 {
     numero: 39,
     titre: "Pour acheter ou vendre un cheval, une paire de bœufs, une vache, etc.",
-    audio: "039.mp3"
+    driveId: "039.mp3"
   },
 {
     numero: 40,
     titre: "Pour acheter volaille, poissons, légumes, fruits au marché",
-    audio: "040.mp3"
+    driveId: "040.mp3"
   },
   {
     numero: 41,
     titre: "Pour acheter des meubles",
-    audio: "041.mp3"
+    driveId: "041.mp3"
   },
   {
     numero: 42,
     titre: "Pour acheter des vêtements",
-    audio: "042.mp3"
+    driveId: "042.mp3"
   },
    {
     numero: 43,
     titre: "Pour acheter une montre et de la bijouterie",
-    audio: "043.mp3"
+    driveId: "043.mp3"
   },
 {
     numero: 44,
     titre: "Pour faire quelque commande à un tailleur, à une couturière",
-    audio: "044.mp3"
+    driveId: "044.mp3"
   },
 {
     numero: 45,
     titre: "Idem. A un cordonnier",
-    audio: "045.mp3"
+    driveId: "045.mp3"
   },
   {
     numero: 46,
     titre: "Idem. A une blanchisseuse",
-    audio: "046.mp3"
+    driveId: "046.mp3"
   },
    {
     numero: 47,
     titre: "Idem. A un ouvrier agricole",
-    audio: "047.mp3"
+    driveId: "047.mp3"
   },
 {
     numero: 48,
     titre: "Idem. A un perruquier",
-    audio: "048.mp3"
+    driveId: "048.mp3"
   },
 {
     numero: 49,
     titre: "Idem. A un garçon, à une fille, qui veut se placer quelque part en qualité de domestique",
-    audio: "049.mp3"
+    driveId: "049.mp3"
   },
 {
     numero: 50,
     titre: "Idem. A un médecin, à un chirurgien",
-    audio: "050.mp3"
+    driveId: "050.mp3"
   },
 {
     numero: 51,
     titre: "Lettres, billets d'invitation",
-    audio: "051.mp3"
+    driveId: "051.mp3"
   },
   {
     numero: 52,
     titre: "L. B. de demande",
-    audio: "052.mp3"
+    driveId: "052.mp3"
   },
    {
     numero: 53,
     titre: "L. B. de remerciements",
-    audio: "053.mp3"
+    driveId: "053.mp3"
   },
 {
     numero: 54,
     titre: "L. B. du premier de l'an à un bienfaiteur",
-    audio: "054.mp3"
+    driveId: "054.mp3"
   },
 {
     numero: 55,
     titre: "L. B. de félicitation pour une nomination",
-    audio: "055.mp3"
+    driveId: "055.mp3"
   },
   {
     numero: 56,
     titre: "L. B. de condoléance dans toutes sortes d'afflictions",
-    audio: "056.mp3"
+    driveId: "056.mp3"
   },
    {
     numero: 57,
     titre: "L. B. de reproche de n'avoir pas reçu de réponse",
-    audio: "057.mp3"
+    driveId: "057.mp3"
   },
 {
     numero: 58,
     titre: "L. B. d'excuse de n'avoir pas répondu",
-    audio: "058.mp3"
+    driveId: "058.mp3"
   },
 {
     numero: 59,
     titre: "L. B. de recommandation pour procurer une place à un jeune homme",
-    audio: "059.mp3"
+    driveId: "059.mp3"
   },
 {
     numero: 60,
     titre: "L. B. un père à un ami, pour recommander un enfant partant pour l'Amérique",
-    audio: "060.mp3"
+    driveId: "060.mp3"
   },
 {
     numero: 61,
     titre: "L. B. pour donner des conseils",
-    audio: "061.mp3"
+    driveId: "061.mp3"
   },
 {
     numero: 62,
     titre: "Proverbes basques",
-    audio: "062.mp3"
+    driveId: "062.mp3"
   }
 ];
