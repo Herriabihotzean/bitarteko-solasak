fdfd4f8ccdc6b6cd5698a877384791af4126ec0e
Herriabihotzean <herriabihotzean64@gmail.com>
2026-07-12 15:24:32 +0200

    Fix syntax error in conversations.js

--- conversations.js
+++ conversations.js
@@ -3,8 +3,7 @@ const conversations = [
     numero: 1,
     titre: "Du départ et du voyage",
     driveId: "1qGwPTnMxqavwOpEGbB56YZotaO7I_XiU"
-  }]
-  ,
+  },
   {
     numero: 2,
     titre: "Conversation en chemin de fer",
